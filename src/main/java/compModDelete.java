8ed555a554ddb58c51e3ec05f0c4af75e9d1015d
kotozayac <anton.piscun@gmail.com>
2021-10-27 15:26:55 +0300

    new class

--- src/main/java/compModDelete.java
+++ src/main/java/compModDelete.java
@@ -1,2 +1,34 @@
-public class compModDelete {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class compModDelete extends compensationModule {
+    public static void main(String[] args) throws Exception{
+        delete();
+    }
+
+    public static void delete() throws Exception {
+        navigateToCompMod();
+        Thread.sleep(5000);
+        List<WebElement> el = driver.findElements(By.xpath("//div[@class='con-vs-tooltip ml-2']/button"));
+        el.get(0).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//button[@class='vs-component vs-button vs-dialog-accept-button vs-button-danger vs-button-filled']")).click();
+        Thread.sleep(5000);
+        el.get(2).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//button[@class='vs-component vs-button vs-dialog-accept-button vs-button-danger vs-button-filled']")).click();
+        Thread.sleep(5000);
+        el.get(4).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//button[@class='vs-component vs-button vs-dialog-accept-button vs-button-danger vs-button-filled']")).click();
+        Thread.sleep(5000);
+        el.get(5).click();
+        Thread.sleep(2000);
+        driver.findElement(By.xpath("//button[@class='vs-component vs-button vs-dialog-accept-button vs-button-danger vs-button-filled']")).click();
+        Thread.sleep(5000);
+
+        driver.close();
+    }
 }
--- src/main/java/compensationModule.java
+++ src/main/java/compensationModule.java
@@ -1,16 +1,39 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.io.*;
 
 public class compensationModule {
-    public static void main(String[] args) throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
+
+    public static WebDriver driver = new ChromeDriver();
+
+    public static void navigateToCompMod() throws Exception {
         System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
         driver.manage().window().maximize();
 
         driver.get("http://localhost/");
-        driver.findElement(By.name("email")).sendKeys("Anton");
-        driver.findElement(By.name("password")).sendKeys("9JmVxzrQk5hi8V6");
+        String[] credent = credentials();
+        driver.findElement(By.name("email")).sendKeys(credent[0]);
+        driver.findElement(By.name("password")).sendKeys(credent[1]);
         driver.findElement(By.name("button")).click();
+        Thread.sleep(5000);
+        WebElement mainMenu = driver.findElement(By.xpath("//a[@href='/compensation']"));
+        Actions action = new Actions(driver);
+        action.moveToElement(mainMenu).perform();
+        Thread.sleep(5000);
+        WebElement compMod = driver.findElement(By.xpath("//span[contains(text(),'Compensation')]"));
+        action.moveToElement(compMod).click().build().perform();
+    }
+
+    public static String[] credentials() throws Exception {
+        File file = new File("/home/anton/Desktop/hs-cm/credentials.txt");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String[] cred = new String[2];
+        cred[0] = br.readLine();
+        cred[1] = br.readLine();
+        return cred;
     }
 }
