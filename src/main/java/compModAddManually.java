7bcb4704e05b3a623882ed5ec92140a18607ed17
kotozayac <anton.piscun@gmail.com>
2021-11-09 13:33:14 +0200

    new chages

--- src/main/java/compModAddManually.java
+++ src/main/java/compModAddManually.java
@@ -1,4 +1,3 @@
-import org.asynchttpclient.util.Assertions;
 import org.openqa.selenium.By;
 
 import java.math.BigDecimal;
@@ -29,19 +28,19 @@ public class compModAddManually extends compensationModule {
         driver.findElement(By.xpath("//h3[text()='Add KZS']/ancestor::div[@class='vs-popup']//span[text()='End date']/..//input[@placeholder='Select Date']")).click();
         Thread.sleep(2000);
         try {
-            driver.findElement(By.xpath("//h3[text()='Add KZS']/ancestor::div[@class='vs-popup']//span[@class='cell day' and text()='31']")).click();
+            driver.findElement(By.xpath("(//h3[text()='Add KZS']/ancestor::div[@class='vs-popup']//span[@class='cell day' and text()='30'])[2]")).click();
         } catch (Exception e) {
             System.out.println(e);
         }
         try {
-            driver.findElement(By.xpath("(//h3[text()='Add KZS']/ancestor::div[@class='vs-popup']//span[contains((@class),'cell day weekend') and text()='31'])[2]")).click();
+            driver.findElement(By.xpath("(//h3[text()='Add KZS']/ancestor::div[@class='vs-popup']//span[contains((@class),'cell day weekend') and text()='30'])[2]")).click();
         } catch (Exception e) {
             System.out.println(e);
         }
         Thread.sleep(2000);
         driver.findElement(By.xpath("//input[@aria-autocomplete='list']")).sendKeys("Agen");
         Thread.sleep(8000);
-        String medName = driver.findElement(By.xpath("//li[@id='vs1__option-1']")).getText();
+        String medName = driver.findElement(By.xpath("//li[@id='vs1__option-1']")).getText().split("\\(")[0];
         driver.findElement(By.xpath("//li[@id='vs1__option-1']")).click();
         Thread.sleep(2000);
         driver.findElement(By.xpath("//h3[text()='Add KZS']/ancestor::div[@class='vs-popup']//span[text()='Medication indicator']/..//input")).click();
@@ -62,20 +61,28 @@ public class compModAddManually extends compensationModule {
         double val2 = roundVal(val50);
         String value2 = Double.toString(val2);
         System.out.println("50% compensation: " + value2);
-        String comp75 = driver.findElement(By.xpath("//span[contains(text(),' CEFZIL ÕHUK. POLÜM.KAT. TBL 250MG N10 ')]/../following::td[2]//span[contains(text(),'75%')]/following::span[1]")).getText().replace("€","");
+        String comp75 = driver.findElement(By.xpath("//span[contains(text(),'" + medName + "')]/../following::td[2]//span[contains(text(),'75%')]/following::span[1]")).getText().replace("€","");
         if (value1.equals(comp75)) {
             System.out.println("Expected value: " + value1 + "\nActual value: " + comp75);
         } else {
             System.out.println("Values does not match!\nExpected value: " + value1 + "\nActual value: " + comp75);
             throw new Exception();
         }
+        String comp50 = driver.findElement(By.xpath("//span[contains(text(),'" + medName + "')]/../following::td[2]//span[contains(text(),'50%')]/following::span[1]")).getText().replace("€","");
+        if (value2.equals(comp50)) {
+            System.out.println("Expected value: " + value2 + "\nActual value: " + comp50);
+        } else {
+            System.out.println("Values does not match!\nExpected value: " + value2 + "\nActual value: " + comp50);
+            throw new Exception();
+        }
+
+        driver.close();
     }
 
     private static double roundVal(double value) {
         BigDecimal bd = new BigDecimal(Double.toString(value));
         bd = bd.setScale(2, RoundingMode.HALF_UP);
         double val = bd.doubleValue();
-        System.out.println("75% of ref price: " + val);
         return val;
     }
 }
